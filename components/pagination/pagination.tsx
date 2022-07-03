e4a9c936e5ebbe7dbb8c5e7dc3ffe0bfdaa5f8aa
Jacob Porciuncula <jacobporci@gmail.com>
2022-07-03 11:05:10 +0800

    Add pagination buttons

--- components/pagination/pagination.tsx
+++ components/pagination/pagination.tsx
@@ -1,18 +1,12 @@
+import { useState } from "react";
+
 export default function Pagination() {
+  const [currPage, setCurrPage] = useState(1);
   return (
     <div className="pagination">
-      <ul>
-        <li className="active">1</li>
-        <li>2</li>
-        <li>3</li>
-        <li>4</li>
-        <li>5</li>
-        <li>6</li>
-        <li>7</li>
-        <li>8</li>
-        <li>9</li>
-        <li>10</li>
-      </ul>
+      <button onClick={() => setCurrPage(currPage - 1)}>Prev</button>
+      <div>{currPage}</div>
+      <button onClick={() => setCurrPage(currPage + 1)}>Next</button>
     </div>
   );
 }
