83f1487e93da3048f85a5bea86b15981e320e256
Jacob Porciuncula <jacobporci@gmail.com>
2022-07-03 13:58:16 +0800

    Add static sorting

--- components/table/table.tsx
+++ components/table/table.tsx
@@ -1,20 +1,68 @@
+import { useState } from "react";
 import { StrippedArticle } from "../../pages";
 
+type SortHeaderProps = {
+  header: string;
+  sorted: boolean;
+  handleSorting: (header: string) => void;
+};
 type Props = {
   items: Array<StrippedArticle>;
 };
+
 export default function Table({ items }: Props) {
+  const [sortedItems, setSortedItems] = useState(items);
+  const [sortKey, setSortKey] = useState<keyof StrippedArticle>();
+  const [sortOrder, setSortOrder] = useState("asc");
+
+  const handleSorting = (sortKey: keyof StrippedArticle, reverse: boolean) => {
+    setSortKey(sortKey);
+    let sortedItems = items.sort((a, b) => (a[sortKey] < b[sortKey] ? -1 : 1));
+
+    if (reverse) {
+      sortedItems.reverse();
+      setSortOrder("desc");
+    }
+
+    setSortedItems(sortedItems);
+  };
+
   return (
     <table>
       <thead>
         <tr>
-          <td>Author</td>
-          <td>Title</td>
-          <td>Date</td>
+          {Object.keys(items[0]).map((item) => {
+            const isSorted = sortKey === item;
+
+            return (
+              <th key={item}>
+                <button
+                  onClick={() => {
+                    handleSorting(
+                      item as keyof StrippedArticle,
+                      isSorted && sortOrder === "asc"
+                    );
+                    setSortOrder(
+                      isSorted && sortOrder === "asc" ? "desc" : "asc"
+                    );
+                  }}
+                  style={{
+                    textTransform: "capitalize",
+                    display: "flex",
+                    alignItems: "center",
+                  }}
+                >
+                  {`${item} ${
+                    isSorted ? (sortOrder === "asc" ? " ▼" : " ▲") : ""
+                  }`}
+                </button>
+              </th>
+            );
+          })}
         </tr>
       </thead>
       <tbody>
-        {items.map(({ author, title, publishedAt }) => {
+        {sortedItems?.map(({ author, title, publishedAt }) => {
           return (
             <tr key={publishedAt}>
               <td>{author}</td>
