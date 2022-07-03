28b1bf556fb11556f22e34892572c2b7a76ea04c
Jacob Porciuncula <jacobporci@gmail.com>
2022-07-03 11:05:17 +0800

    Add label for searchbox

--- components/search/search.tsx
+++ components/search/search.tsx
@@ -1,7 +1,8 @@
 export default function Search() {
   return (
     <div>
-      <h1>Search</h1>
+      <label htmlFor="search">Search:&nbsp;</label>
+      <input id="search" type="search" />
     </div>
   );
 }
