b422e27e5ce274ddd74576293acfae42ca223a49
Jacob Porciuncula <jacobporci@gmail.com>
2022-07-03 11:02:42 +0800

    Integrate with News API + Add types

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,27 @@ import Search from "../components/search/search";
 import Table from "../components/table/table";
 import styles from "../styles/Home.module.css";
 
-const Home: NextPage = () => {
+type Articles = {
+  source: Object;
+  author: string;
+  title: string;
+  description: string;
+  url: string;
+  urlToImage: string;
+  publishedAt: string;
+  content: string;
+};
+
+export type StrippedArticle = Pick<
+  Articles,
+  "author" | "title" | "publishedAt"
+>;
+
+type Props = {
+  articles: Array<StrippedArticle>;
+};
+
+const Home: NextPage<Props> = ({ articles }) => {
   return (
     <div className={styles.container}>
       <Head>
@@ -15,11 +35,28 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <Search />
-        <Table />
+        <Table items={articles} />
         <Pagination />
       </main>
     </div>
   );
+};
+
+export const getStaticProps = async () => {
+  const pageSize = 10;
+  const reponse = await fetch(
+    `https://newsapi.org/v2/everything?q=Apple&sortBy=popularity&pageSize=${pageSize}&apiKey=bf84d7431fa646c8922231ab4935ad89`
+  );
+  const { articles } = await reponse.json();
+  const strippedOutArticles = articles.map(
+    ({ author, title, publishedAt }: Articles) => ({
+      author,
+      title,
+      publishedAt,
+    })
+  );
+
+  return { props: { articles: strippedOutArticles } };
 };
 
 export default Home;
