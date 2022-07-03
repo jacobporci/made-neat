8ad0b826b0afaf08d0fca17035a57f058cbb989f
Jacob Porciuncula <jacobporci@gmail.com>
2022-07-03 11:08:08 +0800

    Add items per page dropdown

--- components/pagination/pagination.tsx
+++ components/pagination/pagination.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 
+const DEFAULT_OPTIONS = [10, 20, 30, 40, 50];
 export default function Pagination() {
   const [currPage, setCurrPage] = useState(1);
   return (
@@ -7,6 +8,14 @@ export default function Pagination() {
       <button onClick={() => setCurrPage(currPage - 1)}>Prev</button>
       <div>{currPage}</div>
       <button onClick={() => setCurrPage(currPage + 1)}>Next</button>
+      <label htmlFor="items-per-page">Items per page:&nbsp;</label>
+      <select id="items-per-page">
+        {DEFAULT_OPTIONS.map((option) => (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
     </div>
   );
 }
