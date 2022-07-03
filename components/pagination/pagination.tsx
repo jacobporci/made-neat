1f56eed4e2c96ccb1e43d8ecdb5a032625401d97
Jacob Porciuncula <jacobporci@gmail.com>
2022-07-03 11:14:21 +0800

    Add some spacings and alignments to pagination component

--- components/pagination/pagination.tsx
+++ components/pagination/pagination.tsx
@@ -1,13 +1,16 @@
 import { useState } from "react";
+import styles from "./pagination.module.css";
 
 const DEFAULT_OPTIONS = [10, 20, 30, 40, 50];
 export default function Pagination() {
   const [currPage, setCurrPage] = useState(1);
   return (
-    <div className="pagination">
-      <button onClick={() => setCurrPage(currPage - 1)}>Prev</button>
-      <div>{currPage}</div>
-      <button onClick={() => setCurrPage(currPage + 1)}>Next</button>
+    <div className={styles.container}>
+      <div className={styles.pagination}>
+        <button onClick={() => setCurrPage(currPage - 1)}>{"<<"}</button>
+        <div className={styles.currentPage}>{currPage}</div>
+        <button onClick={() => setCurrPage(currPage + 1)}>{">>"}</button>
+      </div>
       <label htmlFor="items-per-page">Items per page:&nbsp;</label>
       <select id="items-per-page">
         {DEFAULT_OPTIONS.map((option) => (
