c5b46c6db4428182480b776b14684583f0ff9f65
Jacob Porciuncula <jacobporci@gmail.com>
2022-07-03 11:02:49 +0800

    Render news api response in table

--- components/table/table.tsx
+++ components/table/table.tsx
@@ -1,7 +1,29 @@
-export default function Table() {
+import { StrippedArticle } from "../../pages";
+
+type Props = {
+  items: Array<StrippedArticle>;
+};
+export default function Table({ items }: Props) {
   return (
-    <div>
-      <h1>Table</h1>
-    </div>
+    <table>
+      <thead>
+        <tr>
+          <td>Author</td>
+          <td>Title</td>
+          <td>Date</td>
+        </tr>
+      </thead>
+      <tbody>
+        {items.map(({ author, title, publishedAt }) => {
+          return (
+            <tr key={publishedAt}>
+              <td>{author}</td>
+              <td>{title}</td>
+              <td>{publishedAt}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
   );
 }
